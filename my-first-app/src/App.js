a0475d5c48c800fdc8ee11a6f8e502a7adbf2cff
Azael Bivihou <azaelbivihou1@gmail.com>
2023-06-18 02:54:34 +0100

    partie 4 fin du jeu tic tac toe

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -12,15 +12,32 @@ function Square({ value, onSquareClick}){
     )
 }
 const App = () => {
+  const [xIsNext,setXIsNext]=useState(true);
   const [squares, setSquares]=useState(Array(9).fill(null));
-
+  
+  const gagnant= calculateWinner(squares);
+  let status;
+  if(gagnant){
+    status="le gagnant est:" + gagnant;
+  }else{
+    status="le prochain joueur:" + (xIsNext ? "X":"O");
+  }
   function handleclick(i){
+    if (squares[i] || calculateWinner(squares)){
+      return;
+    }
     const nextsquares=squares.slice();
-    nextsquares[i]="X";
+    if(xIsNext){
+      nextsquares[i]="X";
+    }else{
+      nextsquares[i]="O";
+    }
     setSquares(nextsquares);
+    setXIsNext(!xIsNext);
   }
   return (
     <>
+    <div className='status'>{status}</div>
      <div className='board-row'>
       <Square value={squares[0]} onSquareClick={()=> handleclick(0)}/>
       <Square value={squares[1]} onSquareClick={()=> handleclick(1)}/>
@@ -41,4 +58,27 @@ const App = () => {
 }
 
 export default App
+
+function calculateWinner(squares){
+  const lines=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,1,2],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for(let i=0; i<lines.length;i++){
+    const [a,b,c]=lines[i];
+
+    if(squares[a] && squares[a]===squares[b] && squares[a]===squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
 
