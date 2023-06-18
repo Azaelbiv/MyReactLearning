b2c8e6ba06c9617346f8b9cdd600a45d736a53d2
Azael Bivihou <azaelbivihou1@gmail.com>
2023-06-18 01:53:26 +0100

    partie 3: l'utilisation des fonction qui permet d'inserer la valeur X lorsque l'utilisateur clique sur une case du tableau

--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -2,31 +2,39 @@ import React from 'react'
 import { useState } from 'react';
 import './App.css';
 
-function Square({ value}){
+function Square({ value, onSquareClick}){
 
   return(
     <button
-    className='square'>{value}</button>
+    className='square' onClick={onSquareClick}>
+      {value}
+    </button>
     )
 }
 const App = () => {
   const [squares, setSquares]=useState(Array(9).fill(null));
+
+  function handleclick(i){
+    const nextsquares=squares.slice();
+    nextsquares[i]="X";
+    setSquares(nextsquares);
+  }
   return (
     <>
      <div className='board-row'>
-      <Square value={squares[0]}/>
-      <Square value={squares[1]}/>
-      <Square value={squares[2]}/>
+      <Square value={squares[0]} onSquareClick={()=> handleclick(0)}/>
+      <Square value={squares[1]} onSquareClick={()=> handleclick(1)}/>
+      <Square value={squares[2]} onSquareClick={()=> handleclick(2)}/>
      </div>
      <div className='board-row'>
-      <Square value={squares[3]}/>
-      <Square value={squares[4]}/>
-      <Square value={squares[5]}/>
+      <Square value={squares[3]} onSquareClick={()=> handleclick(3)}/>
+      <Square value={squares[4]} onSquareClick={()=> handleclick(4)}/>
+      <Square value={squares[5]} onSquareClick={()=> handleclick(5)}/>
      </div>
      <div className='board-row'>
-      <Square value={squares[6]}/>
-      <Square value={squares[7]}/>
-      <Square value={squares[8]}/>
+      <Square value={squares[6]} onSquareClick={()=> handleclick(6)}/>
+      <Square value={squares[7]} onSquareClick={()=> handleclick(7)}/>
+      <Square value={squares[8]} onSquareClick={()=> handleclick(8)}/>
      </div>
     </>
   )
